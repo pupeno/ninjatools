fae1780c1a1d371447db81af25a5d22d7bc15897
J. Pablo Fernández <pupeno@carouselapps.com>
2015-08-04 12:22:41 +0100

    Remove queries that we are not using for now.

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -12,19 +12,3 @@ FROM tools
 INSERT INTO tools
 (name, slug, url, description)
 VALUES (:name, :slug, :url, :description)
-
--- name: update-user!
--- update an existing user record
-UPDATE users
-SET first_name = :first_name, last_name = :last_name, email = :email
-WHERE id = :id
-
--- name: get-user
--- retrieve a user given the id.
-SELECT * FROM users
-WHERE id = :id
-
--- name: delete-user!
--- delete a user given the id
-DELETE FROM users
-WHERE id = :id
