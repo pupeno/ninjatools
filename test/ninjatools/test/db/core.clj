6938ee5b97b25bb0e925840964949fbb80545034
J. Pablo Fernández <pupeno@carouselapps.com>
2015-08-06 11:59:14 +0100

    Per-test automatic transactions (not run even once yet, not sure if working, the tests are not).

--- test/ninjatools/test/db/core.clj
+++ test/ninjatools/test/db/core.clj
@@ -2,34 +2,39 @@
 
 (ns ninjatools.test.db.core
   (:require [ninjatools.db.core :as db]
+            [conman.core :as conman]
             [ninjatools.db.migrations :as migrations]
             [clojure.test :refer :all]
             [clojure.java.jdbc :as jdbc]
             [conman.core :refer [with-transaction]]
             [environ.core :refer [env]]))
 
-(use-fixtures
-  :once
-  (fn [f]
-    (db/connect!)
-    (migrations/migrate ["migrate"])
-    (f)))
+(defn set-up-database-test []
+  (use-fixtures :once (fn [f]
+                        (db/connect!)
+                        (migrations/migrate ["migrate"])
+                        (f)))
+  (use-fixtures :each (fn [test-function]
+                        (conman/with-transaction
+                          [t-conn db/conn]
+                          (jdbc/db-set-rollback-only! t-conn)
+                          (test-function)))))
+
+(set-up-database-test)
 
 (deftest test-users
-  (with-transaction [t-conn db/conn]
-    (jdbc/db-set-rollback-only! t-conn)
-    (is (= 1 (db/create-user!
-               {:id         "1"
-                :first_name "Sam"
-                :last_name  "Smith"
-                :email      "sam.smith@example.com"
-                :pass       "pass"})))
-    (is (= [{:id         "1"
-             :first_name "Sam"
-             :last_name  "Smith"
-             :email      "sam.smith@example.com"
-             :pass       "pass"
-             :admin      nil
-             :last_login nil
-             :is_active  nil}]
-           (db/get-user {:id "1"})))))
+  (is (= 1 (db/create-user!
+             {:id         "1"
+              :first_name "Sam"
+              :last_name  "Smith"
+              :email      "sam.smith@example.com"
+              :pass       "pass"})))
+  (is (= [{:id         "1"
+           :first_name "Sam"
+           :last_name  "Smith"
+           :email      "sam.smith@example.com"
+           :pass       "pass"
+           :admin      nil
+           :last_login nil
+           :is_active  nil}]
+         (db/get-user {:id "1"}))))
