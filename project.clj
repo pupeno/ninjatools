aea88b4eecda8b8620922b2a027f4c00cc4050b2
J. Pablo Fernández <pupeno@carouselapps.com>
2015-09-17 14:15:13 +0100

    Use the latest stable version of ClojureScript (and get rid of :scope, it doesn’t do anything anymore).

--- project.clj
+++ project.clj
@@ -29,7 +29,7 @@
                  [conman "0.1.7"]
                  [to-jdbc-uri "0.3.0"]
                  [org.postgresql/postgresql "9.3-1102-jdbc41"]
-                 [org.clojure/clojurescript "1.7.122" :scope "provided"]
+                 [org.clojure/clojurescript "1.7.48"]
                  [org.clojure/tools.reader "0.9.2"]
                  [reagent "0.5.0"]
                  [cljsjs/react "0.13.3-1"]
