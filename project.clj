fc41aa76ba261e4bf66d742d75abf700464f7783
J. Pablo Fernández <pupeno@carouselapps.com>
2015-09-23 12:32:43 +0100

    Correct version number for prerenderer now that it’s in clojars.

--- project.clj
+++ project.clj
@@ -43,7 +43,7 @@
                  [metosin/compojure-api "0.23.1"]
                  [metosin/ring-swagger-ui "2.1.2"]
                  [org.immutant/web "2.1.0"]
-                 [com.carouselapps/prerenderer "0.1.0-SNAPSHOT"]]
+                 [com.carouselapps/prerenderer "0.1.0"]]
 
   :min-lein-version "2.0.0"
   :uberjar-name "ninjatools.jar"
