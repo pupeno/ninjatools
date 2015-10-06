b7704c52eeb884de5e17db9b5f681eede1cd5eb6
J. Pablo Fernández <pupeno@carouselapps.com>
2015-10-06 17:51:18 +0100

    Code formatting.

--- src/cljs/ninjatools/core.cljs
+++ src/cljs/ninjatools/core.cljs
@@ -1,18 +1,18 @@
 ;;;; Copyright © 2015 Carousel Apps, Ltd. All rights reserved.
 
 (ns ninjatools.core
-    (:require [reagent.core :as reagent]
-              [re-frame.core :as re-frame]
-              [ninjatools.handlers]
-              [ninjatools.subs]
-              [ninjatools.routes :as routes]
-              [ninjatools.views :as views]))
+  (:require [reagent.core :as reagent]
+            [re-frame.core :as re-frame]
+            [ninjatools.handlers]
+            [ninjatools.subs]
+            [ninjatools.routes :as routes]
+            [ninjatools.views :as views]))
 
 (defn mount-root []
   (reagent/render [views/main-panel]
                   (.getElementById js/document "app")))
 
-(defn ^:export init [] 
+(defn ^:export init []
   (routes/start!)
   (re-frame/dispatch-sync [:initialize-db])
   (mount-root))
