8c5427541a5451e0542f8386d6f1cd0cf7696c08
J. Pablo Fernández <pupeno@carouselapps.com>
2015-10-19 09:33:15 +0100

    Pass the target of a UI event to the handler. That is, the form or link.
    This allows a handler, for example, to reset the form after it was submitted.

--- src/cljs/ninjatools/ui.cljs
+++ src/cljs/ninjatools/ui.cljs
@@ -4,10 +4,15 @@
   (:require [re-frame.core :as re-frame]
             [reagent.core :as reagent]))
 
-(defn dispatch [event & args]
-  (.preventDefault event)
+(defn dispatch
+  "Re-frame dispatch enhanced for UI. On top of re-frame-dispatching args, it:
+  * stops the default handling from happening, so links and forms don't get submitted.
+  * it dispatches ui-interaction, currently used to hide old alerts
+  * passes the target of the event (the form or link) as the last argument to the re-frame-dispatch."
+  [js-event event]
+  (.preventDefault js-event)
   (re-frame/dispatch [:ui-interaction])
-  (apply re-frame/dispatch args))
+  (re-frame/dispatch (conj event (.-target js-event))))
 
 ; TODO: implement a way for many functions to be called when ui-interaction is dispatched. Right now we don't need  this as we have only one event handler in alerts.cljs.
 
