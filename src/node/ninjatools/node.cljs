2e04652a8563b0dafc19c5a6bb1f7c9b8035dc23
J. Pablo Fernández <pupeno@carouselapps.com>
2015-12-08 15:08:01 +0000

    Use the new prerendering mechanism and re-frame 0.6.0.

--- src/node/ninjatools/node.cljs
+++ src/node/ninjatools/node.cljs
@@ -1,24 +1,21 @@
 ;;;; Copyright © 2015 Carousel Apps, Ltd. All rights reserved.
 
 (ns ninjatools.node
-  (:require [cljs.nodejs :as nodejs]
-            [prerenderer.core :as prerenderer]
+  (:require [prerenderer.core :as prerenderer]
             [prerenderer.re-frame :as re-frame-prerenderer]
+            [re-frame.core :as re-frame]
             ninjatools.core                                 ; To include all of ninja namespaces.
             [ninjatools.routing :as routes]
-            [ninjatools.layout :as views]
-            [reagent.core :as reagent]))
+            [ninjatools.layout :as views])
+  (:require-macros [cljs.core.async.macros :refer [go-loop]]))
 
-(def url (nodejs/require "url"))
 
-(defn render [req res]
-  (let [page-path (.-path (.parse url (.-url (.-query req))))
-        matched-route (routes/parse-path page-path)
+(defn render-and-send [page-path send-to-browser]
+  (let [matched-route (routes/parse-path page-path)
         event (routes/routing-event matched-route)]
     (if (nil? matched-route)
       (throw (js/Error. (str "Unrecognized path: " page-path)))
-      (re-frame-prerenderer/dispatch-super-sync
-        event
-        (fn [] (.send res (reagent/render-to-string [views/main-panel])))))))
+      (do (re-frame/dispatch-sync event)
+          (re-frame-prerenderer/render-by-timeout [views/main-panel] send-to-browser)))))
 
-(set! *main-cli-fn* (prerenderer/create render "Ninja Tools"))
+(set! *main-cli-fn* (prerenderer/create render-and-send "Ninja Tools"))
